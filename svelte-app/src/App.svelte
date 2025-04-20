37371ede399d720d4df33c5b0a5225144113029c
umblumbap <vseos328wrtum@gmail.com>
2025-04-20 16:41:35 +0300

    init

---
+++ svelte-app/src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let name;
+    let count = 0
+
+    function increment() {
+        count+=1
+  }
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>SVo SVo</p>
+    <p>Clicked {count} {count === 1 ? 'time' : 'times'}</p>
+    <button on:click={increment}>Click</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
